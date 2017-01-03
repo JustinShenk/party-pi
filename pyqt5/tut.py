cb841190ddc029cd38002944a73424d1d5b84601
Justin Shenk <shenkjustin@gmail.com>
2017-01-03 14:25:41 +0100

    Update

---
+++ pyqt5/tut.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial
+
+In this example, we create a simple
+window in PyQt5.
+
+author: Jan Bodnar
+website: zetcode.com
+last edited: January 2015
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
