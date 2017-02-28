0b17ee5d68deb7db95911d560325c2c4bb86f7f9
Justin Shenk <shenkjustin@gmail.com>
2017-02-28 22:21:26 +0100

    Add support for direct file upload

--- partypi/uploader.py
+++ partypi/uploader.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import pyimgur
 import json
+import time
+import requests
+
 from emotion_API import Emotion_API
 
 
@@ -20,48 +23,61 @@ def get_conf():
 
 
 class Uploader(object):
+    """Upload files to pyimgur or directly to Microsoft Oxford API."""
 
-    def __init__(self):
+    def __init__(self, local=True):
         self.config = get_conf()
         self.emotion_API = Emotion_API()
-        self.initialize_pyimgur()
+        if local:
+            pass
+        else:
+            self.initialize_pyimgur()
 
     def initialize_pyimgur(self):
         """
         Initialize variables and parameters for PyImgur.
         """
-        _url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
-        _maxNumRetries = 10
         CLIENT_ID = self.config.CLIENT_ID
         CLIENT_SECRET = self.config.CLIENT_SECRET
         self.album = self.config.album
 
-        self.im = pyimgur.Imgur(CLIENT_ID, CLIENT_SECRET)
-        self.im.change_authentication(
-            refresh_token=self.config.refresh_token)
-        self.im.refresh_access_token()
+        # Retry if connection fails
+        ok = False
+        while not ok:
+            try:
+                self.im = pyimgur.Imgur(CLIENT_ID, CLIENT_SECRET)
+                self.im.change_authentication(
+                    refresh_token=self.config.refresh_token)
+                self.im.refresh_access_token()
+                ok = True
+            except requests.exceptions.HTTPError:
+                print("Imgur is temporarily over capacity. Please try again later.")
+                time.sleep(4)
 
         # TODO: Complete Imgur user settings.
         # user = self.config.user
 
-    def upload_img(self, imagepath):
+    def upload_img(self, imagepath, pyimgur=False):
         """
-        Send image to PyImgur.
+        Send local image directly or send image to PyImgur for hosting.
         """
         print("Initate upload")
 
-        uploaded_image = self.im.upload_image(
-            imagepath, title="Uploaded with PyImgur", album=self.album)
+        if pyimgur:
+            uploaded_image = self.im.upload_image(
+                imagepath, title="Uploaded with PyImgur", album=self.album)
 
-        # TODO: Turn on album uploading
-        # uploaded_image = self.im.upload_image(
-        #     self.imagepath, title="Uploaded with PyImgur", album=self.album)
-        print((uploaded_image.title))
-        print((uploaded_image.link))
-        print((uploaded_image.size))
-        print((uploaded_image.type))
+            # TODO: Turn on album uploading
+            # uploaded_image = self.im.upload_image(
+            # self.imagepath, title="Uploaded with PyImgur", album=self.album)
+            print((uploaded_image.title))
+            print((uploaded_image.link))
+            print((uploaded_image.size))
+            print((uploaded_image.type))
 
-        # Send emotion data to API and return to game.
-        data = self.emotion_API.get_emotions(uploaded_image.link)
+            # Send emotion data to API and return to game.
+            data = self.emotion_API.get_emotions(uploaded_image.link)
+        else:
+            data = self.emotion_API.get_emotions(imagepath, local=True)
 
         return data
