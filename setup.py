28f13420f43bdc83466f0d0de2e2ad7df4888dca
Justin Shenk <shenkjustin@gmail.com>
2018-04-08 23:17:14 +0200

    Initial travis commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import re
 import sys
 
 
-VERSION = '0.1.5'  # x.y.z[-dev#]
+VERSION = '0.1.6'  # x.y.z[-dev#]
 REPOSITORY = 'https://github.com/JustinShenk/party-pi'
 
 PACKAGES = find_packages(where='partypi')
@@ -15,6 +15,18 @@ with open('README.rst', 'r') as f:
 README = re.sub(r' _(.+): ([^(http)].+)',
                 r' _\1: {}/blob/master/\2'.format(REPOSITORY), README)
 
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+extra_files = package_files('match_scientist')
+print("Extra_files: ", extra_files)
+
 setup(
     name='partypi',
     version=VERSION,
@@ -22,10 +34,26 @@ setup(
     long_description=README,
     author='Justin Shenk',
     author_email='shenk.justin@gmail.com',
-
+    packages=['partypi']
+    package_data={'': extra_files},
+    install_requires=[
+        'opencv-contrib-python==3.3.0.9',
+        'pillow',
+        'Flask',
+        'Flask-SSLify',
+        'gunicorn',
+        'itsdangerous',
+        'Jinja2',
+        'scipy',
+        'six',
+        'Werkzeug',
+        'keras',
+        'tensorflow',
+        'h5py',
+        'gevent'
+    ]
     url=REPOSITORY,
     download_url='{}/tarball/{}'.format(REPOSITORY, VERSION),
-    packages=PACKAGES,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Education',
